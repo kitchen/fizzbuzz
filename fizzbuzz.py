338981bd948e35a6046b0dc144935a22960db994
Jeremy Kitchen <kitchen@kitchen.io>
2012-02-13 18:47:45 -0800

    explain the range in the python version
    since I'm likely to come back later and look at this and be like "wtf, mate?" I thought I should comment in here as to why, while the rules specify counting from 1 to 100, this appears to go to 101. apparently python's range() function is non-inclusive of the upper bound

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-for i in range(1,101):
+for i in range(1,101): # range doesn't include the second argument
     if i % 3 == 0 and i % 5 == 0:
         print "FizzBuzz"
     elif i % 3 == 0:
