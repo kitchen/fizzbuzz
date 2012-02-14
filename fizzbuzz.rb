94740fcae9aac4be1963314bd78601e2441318ef
Jeremy Kitchen <kitchen@kitchen.io>
2012-02-13 18:23:06 -0800

    ruby version

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+(1..100).each do |i|
+	if i % 3 == 0 && i % 5 == 0 then
+		puts "FizzBuzz"
+	elsif i % 3 == 0 then
+		puts "Fizz"
+	elsif i % 5 == 0 then
+		puts "Buzz"
+	else
+		puts i
+	end
+end
