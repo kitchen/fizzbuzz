ad8f14813200f3525d53938a666d6cb9da24166e
Jeremy Kitchen <kitchen@kitchen.io>
2012-02-19 11:18:17 -0800

    new Erlang, more 'erlang-esque'
    features of this program:
    uses function guards
    uses multiple arity of fizzbuzz function
    uses list comprehensions
    
    can you tell I'm learning erlang? :)

--- fizzbuzz.erl
+++ fizzbuzz.erl
@@ -1,24 +1,19 @@
 % to run this, open a shell and do the following:
 % erl
 % c(fizzbuzz).
-% fizzbuzz:main().
+% fizzbuzz:fizzbuzz().
 -module(fizzbuzz).
--export([main/0]).
+-export([fizzbuzz/0]).
 
-fizzbuzz(Fizz_buzz_counter) when Fizz_buzz_counter > 100 ->
-	ok;
-fizzbuzz(Fizz_buzz_counter) ->
-	if
-		Fizz_buzz_counter rem 3 == 0, Fizz_buzz_counter rem 5 == 0 ->
-			io:format("FizzBuzz~n", []);
-		Fizz_buzz_counter rem 3 == 0 ->
-			io:format("Fizz~n", []);
-		Fizz_buzz_counter rem 5 == 0 ->
-			io:format("Buzz~n", []);
-		true ->
-			io:format("~w~n", [Fizz_buzz_counter])
-	end,
-	fizzbuzz(Fizz_buzz_counter+1).
+fizzbuzz() ->
+	[fizzbuzz(X) || X <- lists:seq(1, 100)].
 
-main() ->
-	fizzbuzz(1).
+fizzbuzz(X) when X rem 3 =:= 0, X rem 5 =:= 0 ->
+	io:format("FizzBuzz~n", []);
+fizzbuzz(X) when X rem 3 =:= 0 ->
+	io:format("Fizz~n", []);
+fizzbuzz(X) when X rem 5 =:= 0 ->
+	io:format("Buzz~n", []);
+fizzbuzz(X) ->
+	io:format("~w~n", [X]).
+
